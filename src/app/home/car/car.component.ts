0af9f19cdedcbaf7eeeb7d04446247d55faa197a
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-02-23 21:58:48 +0200

    add homeComponent, carComponent, car  service and car model

---
+++ src/app/home/car/car.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Car } from './models/car-model';
+
+@Component({
+  selector: '.car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+  @Input() car: Car = {
+    id: 0,
+    brand:"",
+    model: "",
+    weight: 0,
+    isAvailable: false
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Car } from './car/models/car-model';
+import {Observable, Subscription, throwError} from "rxjs";
+import {CarService} from "../services/car.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit ,OnDestroy {
+
+  public cars:Car[] = [];
+
+  public subscription= new Subscription();
+  constructor(private service: CarService) { }
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+
+  getCars(){
+
+    this.subscription.add(
+      this.service.getCars().subscribe({
+      next: (cars) => this.cars = cars,
+      error: err => throwError(err)
+    }))
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+
+}
